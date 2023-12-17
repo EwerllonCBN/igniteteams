d1f4c0f82de39a8ab539886f745a4f3b7825af0f
ewerlloncbn <ewerllon_cristian@hotmail.com>
2023-12-16 22:09:30 -0400

    :recycle: refactor - excluded consoles and inutilized imports in the files projects.

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,5 +1,5 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
-import styled, { css } from 'styled-components/native'
+import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
